b1a9563a1a3b3079a7d32b6d282e7ade9ea8ebab
tayyab5838 <tayyabhussain@gmail.com>
2025-01-06 12:34:44 +0500

    second commit

---
+++ src/react_agent/prompts.py
@@ -0,0 +1,7 @@
+"""Default prompts used by the agent."""
+
+SYSTEM_PROMPT = """"You are a helpful assistant that provide answers to the questions using tools that provided 
+to you. If user ask about the informations that you don't have use tool to get browse the internet to get
+relevant informations.
+
+System time: {system_time}"""
